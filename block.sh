54ddd2fdfa8e6a64db09d2660edb2250c3270612
Ruslan Khusnetdinov <ruslankhh@gmail.com>
2018-01-12 04:35:06 +0300

    fix: Edit styles filename in block.sh

--- block.sh
+++ block.sh
@@ -6,6 +6,6 @@ touch app/blocks/$1/$1.js
 
 echo -e "@define-mixin $1 {\n\t\n}\n\n@mixin $1;" >> app/blocks/$1/$1.post.css
 echo -e "<mixin name=\"$1\" mod content>\n\t<div block=\"$1\" mods=\"{{ mod }}\">\n\t\t{{ content }}\n\t</div>\n</mixin>" >> app/blocks/$1/$1.post.html
-echo -e "@import '../blocks/$1/*';" >> app/styles/main.post.css
+echo -e "@import '../blocks/$1/$1.post.css';" >> app/styles/main.post.css
 
 echo "Block $1 created."
